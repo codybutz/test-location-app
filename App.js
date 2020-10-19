077f643ac100a8bb241bffd7e3e0ad2389054741
Cody Butz <codyjbutz@gmail.com>
2020-10-19 15:15:44 -0400

    Test build for Github

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as Location from 'expo-location';
+import * as TaskManager from "expo-task-manager";
+import * as Permissions from 'expo-permissions';
+
+export default class App  extends Component {
+    async startTracking() {
+        const permission = await Permissions.askAsync(Permissions.LOCATION);
+        if (permission.status === 'granted') {
+
+            console.log('Attempting to start tracking.');
+            console.log(TaskManager.isTaskDefined('test-app-location'));
+            Location.startLocationUpdatesAsync('test-app-location', {
+                accuracy: Location.Accuracy.Highest,
+                activityType: Location.ActivityType.AutomotiveNavigation,
+                timeInterval: 1000,
+                distanceInterval: 0,
+                foregroundService: {
+                    notificationTitle: 'Test App Tracking',
+                    notificationBody: 'Currently actively tracking.'
+                },
+                pausesUpdatesAutomatically: false,
+            }).then(() => {
+                console.log('Location tracking - Background task');
+            }).catch((err) => {
+                console.error('Unable to start tracking')
+                console.error(err);
+            });
+        } else {
+            console.log('Unable to get permission...');
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{color: '#000'}}>Ejected app test v2</Text>
+                <TouchableOpacity
+                    style={{backgroundColor: '#004470', borderRadius: 5, padding: 20}}
+                    onPress={() => {this.startTracking()}}
+                >
+                    <Text style={{color: '#fff'}}>Click here to start tracking</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+TaskManager.defineTask(
+    'test-app-location',
+    ({ data, error }) => {
+        console.log('Hello - task received execution');
+        if (error) {
+            // Error occurred - check `error.message` for more details.
+            console.error('Unable to start location task.');
+            console.error(error);
+            return;
+        }
+
+        if (data) {
+            console.log(Date.now());
+        }
+    }
+);
+
+
